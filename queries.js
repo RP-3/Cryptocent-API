cace64a331ef3d061d65830d55f401f528a4d979
Rohan <sarith21@gmail.com>
2014-06-25 21:06:05 -0700

    adds function to fetch dates hourly

--- queries.js
+++ queries.js
@@ -7,14 +7,14 @@ var connection = new sql.Connection(config, function(err){
 	}else{
 		console.log('Database connection established.');
 		//execute test functions during development here:
-		query(testQ, console.log);
+		hourly(testQ, console.log);
 		//^^^
 	}
 });
 
 var testQ = {
 	currency: 'bi',
-	start: '2014-06-25T20:11:08.246Z',
+	start: '2014-06-24T20:11:08.246Z',
 	end: '2014-06-25T20:16:39.868Z'
 };
 
@@ -41,7 +41,6 @@ var queryConstructor = function(queryObj){
 /*standard query to return all results*/
 var query = function(queryObj, cb){
 	var q = queryConstructor(queryObj);
-	console.log(q);
 	var request = connection.request();
 
 	request.query(q, function(err, recordset) {
@@ -52,13 +51,31 @@ var query = function(queryObj, cb){
 	});
 };
 
-
-
-/*return minutely average in date range*/
-
 /*return hourly average in date range*/
+var hourly = function(queryObj, cb){
+	var q = "select CAST(FLOOR(CAST(updated as float)) as datetime) as day, datepart(hh, updated) as hour, avg(ask) as ask, avg(bid) as bid, avg(last) as last from transactions where currency = '"+ queryObj.currency +"' and updated between '"+ new Date(queryObj.start).toISOString() +"' and '"+ new Date(queryObj.end).toISOString() +"' group by cast(FLOOR(CAST(updated as float)) as datetime), datepart(hh, updated) order by cast(FLOOR(CAST(updated as float)) as datetime) asc, datepart(hh, updated) asc";
+	var request = connection.request();
+	request.query(q, function(err, recordset){
+		if(err){
+			console.log(err);
+		}
+		cb(recordset);
+	});
+};
 
-/*return daily average in date range*/
+
+var daily = function(queryObj, cb){
+	/* return daily average in date range
+	SELECT 
+	CAST (updated as date), 
+	AVG(ask) as ask, AVG(bid) as bid, AVG(last) as last
+	from transactions
+	where currency = 'bi'
+	and updated between '2014-06-24' and '2014-06-26'
+	group by cast(updated as date)
+	order by case (updated as date) asc
+	*/
+};
 
 
 /*declare exports*/
