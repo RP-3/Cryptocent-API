f8e166ff6e94dd659513aa8eed1f73d4ef002b82
Rohan <sarith21@gmail.com>
2014-06-26 19:33:53 -0700

    adds config files to app setup and initialises port

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 /*load app basics and create app*/
 var express = require('express');
 var app = express();
+var config = require('./config/config.js');
 
-require('./db.js');
+config(app);
+
+app.listen(process.env.PORT || 3000);
