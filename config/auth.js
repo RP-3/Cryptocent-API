3a1f0927490edce6f2b31504e9ef611e5349870b
Rohan <sarith21@gmail.com>
2014-07-04 18:00:08 +1000

    configures passport for oAuth

--- config/auth.js
+++ config/auth.js
@@ -1,15 +1,22 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google').Strategy;
+var signIn = require('../routes/userQueries.js').signIn;
 
 passport.use(new GoogleStrategy({
     returnURL: 'http://localhost:3000/console',
     realm: 'http://localhost:3000'
   },
   function(identifier, profile, done) {
-    User.findOrCreate({ openId: identifier }, function(err, user) {
-      done(err, user);
-    });
+    signIn(identifier, profile, done); //repeated arguments for readability
   }
 ));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 module.exports = passport;
