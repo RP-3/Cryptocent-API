1e18e60b7bfd69bf3182c23d0f40828fa28420f7
Rohan <sarith21@gmail.com>
2014-07-04 18:03:33 +1000

    initialises sql connection and adds user-creation function for export

--- routes/userQueries.js
+++ routes/userQueries.js
@@ -1,8 +1,30 @@
-/**/
+/*initialise sql connections*/
+var sql = require('mssql');
+var config = require('../config_dev.json').mssql_users;
+var connection = new sql.Connection(config, function(err){
+    if(err){
+        console.log('Users database connection error:');
+        console.log(err);
+        process.exit();
+    }else{
+        console.log('Users database connection established.');
+    }
+});
+
 
 /*sign in*/
-//check if user exists
-    //create user
+var signIn = function(identifier, profile, done){
+    var id = identifier.slice(identifier.indexOf('=')+1),
+    name = profile.name.givenName,
+    email = profile.emails[0],
+    //check if user exists and create if not
+    q = "if not exists (select * from traders where identifier = '"+ id +"') begin insert into traders (identifier, name, bitcoin, dodgecoin, litecoin, usd, email) values ('"+ id +"', '"+ name +"', 1.0, 1.0, 1.0, 1000.0, '"+ email +"') end";
+
+    var request = connection.request();
+    request.query(q, function(err){
+        done(err, id);
+    });
+};
 
 /*buy currency*/
 //check sufficient account balance
@@ -26,3 +48,5 @@
 /*delete user*/
 //delete user from users table
 //find user entries in transaction table and delete them too
+
+module.exports.signIn = signIn;
