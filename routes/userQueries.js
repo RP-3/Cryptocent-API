376e17ce65900a4a61dea9dc2c5c925ccfa20586
Rohan <sarith21@gmail.com>
2014-07-04 15:22:12 +1000

    adds sudocode for user account routing

---
+++ routes/userQueries.js
@@ -0,0 +1,28 @@
+/**/
+
+/*sign in*/
+//check if user exists
+    //create user
+
+/*buy currency*/
+//check sufficient account balance
+//subtract dollar from dollars
+//quant = rate * dollar
+//add quant to currency
+
+/*sell currency*/
+//check sufficient currency balance
+//subtract quant from currency
+//dollar = quant / rate
+//add dollar to dollars
+
+/*create user*/
+//create user entry
+    //save gid, token, 
+    //default to one of each currency && $1,000
+    //last to null
+//save user entry to DB
+
+/*delete user*/
+//delete user from users table
+//find user entries in transaction table and delete them too
