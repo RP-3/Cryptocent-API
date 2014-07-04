f5da2e495c32d88554c42db28e9cd96d4cc74ae6
Rohan <sarith21@gmail.com>
2014-07-04 18:00:52 +1000

    adds standard middleware to support sessions, and a route for future client-side app

--- config/config.js
+++ config/config.js
@@ -1,10 +1,27 @@
 var apiRouter = require('../routes/apiRouter.js');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var passport = require('./auth.js');
 
 module.exports = function(app){
+    /*standard middleware*/
+    app.use(cookieParser());
+    app.use(bodyParser());
+    app.use(session({ secret: 'fooBotFunkyTown'}));
+
+    /*non-protected routes*/
 	app.use('/api', apiRouter);
-	app.get('/', passport.authenticate('google'));
+
+    /*passport-specific middleware*/
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    /*Authenticated requests load client-side app (protected)*/
     app.get('/console', passport.authenticate('google', {
-        successRedirect: '/',
-        failureRedirect: '/fail'}));
+        successRedirect: '/account',
+        failureRedirect: '/'}));
+
+    /*all other requests follow authentication loop*/
+    app.get('/', passport.authenticate('google'));
 };
