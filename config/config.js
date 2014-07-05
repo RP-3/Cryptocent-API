448a6d94e2d6eb5a35eb66cd5586e4a39c23d29c
Rohan <sarith21@gmail.com>
2014-07-05 10:55:48 +1000

    adds user router for handling transactions and clarifies comments in middleware chain to reflect routing structure

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 var apiRouter = require('../routes/apiRouter.js');
+var userRouter = require('../routes/userRouter.js');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
@@ -10,18 +11,19 @@ module.exports = function(app){
     app.use(bodyParser());
     app.use(session({ secret: 'fooBotFunkyTown'}));
 
-    /*non-protected routes*/
+    /*non-passport-protected routes*/
 	app.use('/api', apiRouter);
+    app.use('/api/transact', userRouter);
 
     /*passport-specific middleware*/
     app.use(passport.initialize());
     app.use(passport.session());
 
-    /*Authenticated requests load client-side app (protected)*/
+    /*Authenticated get requests load client-side app (passport-protected)*/
     app.get('/console', passport.authenticate('google', {
-        successRedirect: '/account',
+        successRedirect: '/account', // '/account' should load client-side app
         failureRedirect: '/'}));
 
-    /*all other requests follow authentication loop*/
+    /*all other get requests follow authentication loop*/
     app.get('/', passport.authenticate('google'));
 };
