e87740197359c7d0a523552f9083587f400e8888
Rohan <sarith21@gmail.com>
2014-07-05 12:38:30 +1000

    adds function to validate currency and quantity parameters

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,15 +2,39 @@ var express = require('express');
 var userRouter = express.Router();
 var queries = require('./userQueries.js');
 
-/*set up authentication for router*/
+/*set up id authentication for router*/
 userRouter.use(function(req, res, next){
     console.log(req.body.id);
     if(!req.body.id){
-        res.send(400, 'Transactional requests must include an account id');
+        res.send(403.10, 'Transactional requests must include an account id');
     }else{
         next();
     }
 });
+
+var validateCurrAndQuant = function(req, res, next){
+    var currency = req.body.currency;
+    if(!(currency === 'bitcoin' || currency === 'litecoin' || currency === 'dogecoin')){
+        var badCurr = new Error('Invalid currency parameter. Use "bitcoin", "litecoin" or "dogecoin".');
+        badCurr.status = 403.10;
+        return next(badCurr);
+    }
+
+    var quantity = req.body.quantity;
+    if(!isNaN(parseFloat(quantity)) && parseFloat(quantity) < 1000000000){
+        req.body.quantity = parseFloat(quantity);
+    }else{
+        var badQuant = new Error('quantity must be a valid float  and < a billion');
+        badQuant.status = 403.1;
+        return next(badQuant);
+    }
+
+    next();
+};
+
+/*validate currency and quantity params*/
+userRouter.use('/buy', validateCurrAndQuant);
+userRouter.use('/sell', validateCurrAndQuant);
 
 /*set up routes*/
 userRouter.post('/account', function(req, res){ //get user's account
@@ -25,8 +49,13 @@ userRouter.post('/account', function(req, res){ //get user's account
     });
 });
 
+userRouter.post('/buy', function(req, res){
+    
+});
 
+userRouter.post('/sell', function(req, res){
 
+});
 
 /*declare exports*/
 module.exports = userRouter;
