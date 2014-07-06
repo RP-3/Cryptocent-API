f5604f2df055b56b0d8d8d91059a21e29439414b
Rohan <sarith21@gmail.com>
2014-07-07 09:55:43 +1000

    adds main app module, its controller and its config (containing just one state)

---
+++ public/app/app/app.js
@@ -0,0 +1,14 @@
+angular
+    .module('app', [
+        'ui.router'
+        ])
+    .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+        $urlRouterProvider.otherwise('/'); //seems to control what happens when navigating directly to routes from within the domain of the app
+    
+        //given to us by ui-router
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'app/home/home.html'
+            })
+    }])
