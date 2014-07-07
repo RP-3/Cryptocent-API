46011dd76d499fe2f1c77ca86a42099fe8a1d1e6
Rohan <sarith21@gmail.com>
2014-07-08 06:13:06 +1000

    creates custom directive for rickshaw charts

---
+++ public/app/app/directives/rickshawChart.js
@@ -0,0 +1,33 @@
+angular
+    .module('app')
+    .directive('rickshawChart', function(){
+        return {
+            scope: {
+                data: '=',
+                renderer: '='
+            },
+            template: '<div></div>',
+            restrict: 'E',
+            link: function postLink(scope, element, attrs){
+                scope.$watchCollection('[data, renderer]', function(newVal, oldVal){
+                    if(!newVal[0]){
+                        return;
+                    }
+
+                    var graphEl = element.find("div:first");
+                    graphEl.html('');
+
+                    var graph = new Rickshaw.Graph({
+                        element: graphEl[0],
+                        width: attrs.width,
+                        height: attrs.height,
+                        series: [{data: scope.data, color: attrs.color, name:attrs.series}],
+                        renderer: scope.renderer
+                    });
+
+                    graph.render();
+
+                });
+            }
+        };
+    });
