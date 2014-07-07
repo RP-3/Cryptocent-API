3a807622a29ed68eba9a58f50d9eeebf409086e7
Rohan <sarith21@gmail.com>
2014-07-07 10:50:22 +1000

    adds in a simple ng-options

--- public/app/home/homeController.js
+++ public/app/home/homeController.js
@@ -3,4 +3,5 @@ angular
     .controller('homeController', ['$scope', function($scope){
         $scope.title = 'Home';
         $scope.items = [1, 3, 5, 2, 7];
+        $scope.selectedValue = 5;
     }]);
