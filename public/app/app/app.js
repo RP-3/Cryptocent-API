6ee5cf869e0d0feb714d40249f64e85caf690726
Rohan <sarith21@gmail.com>
2014-07-07 17:08:05 +1000

    adds locationProvider and removes unnecessary resolve in homeUrl

--- public/app/app/app.js
+++ public/app/app/app.js
@@ -2,7 +2,8 @@ angular
     .module('app', [
         'ui.router'
         ])
-    .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+    .config(['$urlRouterProvider', '$stateProvider', '$locationProvider', function($urlRouterProvider, $stateProvider, $locationProvider){
+        $locationProvider.html5Mode(true);
         $urlRouterProvider.otherwise('/'); //seems to control what happens when navigating directly to routes from within the domain of the app
     
         //given to us by ui-router
@@ -10,18 +11,11 @@ angular
             .state('home', {
                 url: '/',
                 templateUrl: 'app/home/home.html',
-                controller: 'homeController',
-                resolve: {
-                    friends: ['$http', function($http){
-                        return $http.get('http://localhost:3000/api/minly/bi/2014-06-25T20:11:08.246Z/2014-06-25T20:16:39.868Z').then(function(response){
-                            return response;
-                        })
-                    }]
-                }
+                controller: 'homeController'
             })
             .state('about', {
                 url: '/about',
                 templateUrl: 'app/about/about.html',
                 controller: 'aboutController'
-            })
-    }])
+            });
+    }]);
